4b2b9a6d2f9042ab68f90b8480fc277114926c34
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-05-07 06:35:54 +0630

    Test 10

--- pkgs/meow/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-__ScriptVersion=1.0.0
-__Author="Aung Myat Moe"
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get --assume-yes upgrade
 
-echo "deb [trusted=yes] https://amm834.github.io/meow/dists termux extras" > $PREFIX/etc/apt/sources.list.d/meow.list
+echo "deb [trusted=yes] https://amm834.github.io/meow/pkgs termux extras" > $PREFIX/etc/apt/sources.list.d/meow.list
 
    
 apt update
